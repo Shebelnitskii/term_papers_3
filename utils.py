609413d7d85da17748839ad03f10d655f6679c82
Shebelnitskii <Shebelnitskiywork@gmail.com>
2023-02-17 15:13:15 +0400

    add gitignore and add function load_json from utils.py

---
+++ utils.py
@@ -0,0 +1,5 @@
+import requests
+
+def load_json():
+    transactions_json = requests.get('https://file.notion.so/f/s/d22c7143-d55e-4f1d-aa98-e9b15e5e5efc/operations.json?spaceId=0771f0bb-b4cb-4a14-bc05-94cbd33fc70d&table=block&id=f11058ed-10ad-42ea-a13d-aad1945e5421&expirationTimestamp=1676715784166&signature=8VqIM33J1Hasw50YJhM1bXPs9HfKgnhSwXhpOXxTUzg&downloadName=operations.json').json()
+    return transactions_json
